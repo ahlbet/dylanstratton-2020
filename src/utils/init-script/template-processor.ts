058ea5df3a1427b8b449949555c82c6c75e65814
Dylan Stratton <dylanstratton@Dylans-MacBook-Pro-3.local>
2025-08-17 21:06:42 -0500

    fix: cover art

--- src/utils/init-script/template-processor.ts
+++ src/utils/init-script/template-processor.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs'
 import * as path from 'path'
 import { transformDate } from '../date-utils'
-import { generateCoverArt } from '../cover-art-generator.js'
+import { generateCoverArt } from '../cover-art-generator'
 
 interface AudioFile {
   fileName: string
